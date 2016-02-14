935a2ba28c4b086032be7b701c94f595f33347ca
Scott Higgins <skoty.higgins@gmail.com>
2016-02-13 19:39:27 -0500

    Complete overhaul of original design. Improved login and registration that utilize the SQLite database.

---
+++ app/src/main/java/edu/gatech/snickers/techflixandchill/Home.java
@@ -0,0 +1,18 @@
+package edu.gatech.snickers.techflixandchill;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Scottie on 2/13/16.
+ *
+ * Home screen of our app itself
+ */
+public class Home extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+// TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
--- app/src/main/java/edu/gatech/snickers/techflixandchill/MainActivity.java
+++ app/src/main/java/edu/gatech/snickers/techflixandchill/MainActivity.java
@@ -1,77 +1,140 @@
 package edu.gatech.snickers.techflixandchill;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+/**
+ * Created on 2/12/16. Last modified on 2/13/16.
+ *
+ * Main activity of our application. Allows users to choose
+ * between logging in and registering an account with the app.
+ */
+public class MainActivity extends Activity {
 
-    Button bLogout;
-    EditText etName, etAge, etMajor, etUsername;
-    //UserLocalStore userLocalStore;
+    LoginDataBaseAdapter loginDataBaseAdapter;
+    Button login;
+    Button registerr;
+    EditText enterpassword;
+    TextView forgetpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUsername = (EditText) findViewById(R.id.etUsername);
-        etName = (EditText) findViewById(R.id.etName);
-        etAge = (EditText) findViewById(R.id.etAge);
-        etMajor = (EditText) findViewById(R.id.etMajor);
+        login=(Button)findViewById(R.id.login_btn);
+        registerr=(Button)findViewById(R.id.register_btn);
+        enterpassword=(EditText)findViewById(R.id.password_edt);
+        forgetpass=(TextView)findViewById(R.id.textView2);
 
-        bLogout = (Button) findViewById(R.id.bLogout);
+        loginDataBaseAdapter = new LoginDataBaseAdapter(getApplicationContext());
+        loginDataBaseAdapter.open();
 
-        bLogout.setOnClickListener(this);
+        registerr.setOnClickListener(new OnClickListener() {
 
-        //userLocalStore = new UserLocalStore(this);
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                Intent i=new Intent(MainActivity.this,Registration.class);
+                startActivity(i);
+            }
+        });
 
+        login.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                String Password = enterpassword.getText().toString();
+
+                String storedPassword = loginDataBaseAdapter.getSinlgeEntry(Password);
+
+                if(Password.equals(storedPassword))
+                {
+                    Toast.makeText(MainActivity.this, "Congrats: Login Successfully", Toast.LENGTH_LONG).show();
+                    Intent ii=new Intent(MainActivity.this,Home.class);
+                    startActivity(ii);
+                }
+                else
+                if(Password.equals("")){
+                    Toast.makeText(MainActivity.this, "Please Enter Your Password", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Password Incorrect", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        forgetpass.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.getWindow();
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.forget_search);
+                dialog.show();
+
+                final EditText security=(EditText)dialog.findViewById(R.id.securityhint_edt);
+                final TextView getpass=(TextView)dialog.findViewById(R.id.textView3);
+
+                Button ok=(Button)dialog.findViewById(R.id.getpassword_btn);
+                Button cancel=(Button)dialog.findViewById(R.id.cancel_btn);
+
+                ok.setOnClickListener(new View.OnClickListener() {
+
+                    public void onClick(View v) {
+
+                        String userName=security.getText().toString();
+                        if(userName.equals(""))
+                        {
+                            Toast.makeText(getApplicationContext(), "Please enter your securityhint", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            String storedPassword=loginDataBaseAdapter.getAllTags(userName);
+                            if(storedPassword==null)
+                            {
+                                Toast.makeText(getApplicationContext(), "Please enter correct securityhint", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Log.d("GET PASSWORD",storedPassword);
+                                getpass.setText(storedPassword);
+                            }
+                        }
+                    }
+                });
+                cancel.setOnClickListener(new OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+// TODO Auto-generated method stub
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-
-        startActivity(new Intent(this, Welcome.class));
-
-        /*if (authenticate() == true) {
-            displayUserDetails();
-        } else {
-            startActivity(new Intent(this, Welcome.class));
-        }*/
+    protected void onDestroy() {
+        super.onDestroy();
+// Close The Database
+        loginDataBaseAdapter.close();
     }
 
-    /*private boolean authenticate() {
-        return userLocalStore.getUserLoggedIn();
-    }
-
-    private void displayUserDetails() {
-        User user = userLocalStore.getLoggedInUser();
-
-        etUsername.setText(user.username);
-        etName.setText(user.name);
-        etAge.setText(user.age + "");
-    }*/
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.bLogout:
-
-                //userLocalStore.clearUserData();
-                //userLocalStore.setUserLoggedIn(false);
-
-                startActivity(new Intent(this, Login.class));
-
-                break;
-        }
-    }
 }
