7353c903384d895268224db3d92343cb2b85e9ff
Niraj Suresh <nsuresh9@gatech.edu>
2016-02-21 21:30:03 -0500

    Ultra mega beast mode commit 2.0

--- app/src/main/java/edu/gatech/snickers/techflixandchill/NewInTheaters.java
+++ app/src/main/java/edu/gatech/snickers/techflixandchill/NewInTheaters.java
@@ -2,18 +2,38 @@ package edu.gatech.snickers.techflixandchill;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class NewInTheaters extends Activity {
 
-    Button newInTheatersReturnHome;
+    Button newInTheatersReturnHome, nextPage, previousPage;
+    TextView rentalText;
+    static int pagenumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_in_theaters);
+        rentalText = (TextView) findViewById(R.id.rentalText);
+        nextPage = (Button) findViewById(R.id.nextPage);
+        previousPage = (Button) findViewById(R.id.previousPage);
+        new JSONTask().execute("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/" +
+                "in_theaters.json?page_limit=15&page=1&country=us&apikey=yedukp76ffytfuy24zsqk7f5");
 
         final Bundle bundle;
         bundle = getIntent().getExtras();
@@ -29,6 +49,82 @@ public class NewInTheaters extends Activity {
                 startActivity(i);
             }
         });
+
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pagenumber++;
+                new JSONTask().execute("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/" +
+                        "in_theaters.json?page_limit=15&page=" + pagenumber +
+                        "&country=us&apikey=yedukp76ffytfuy24zsqk7f5");
+            }
+        });
+
+        previousPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pagenumber > 1) {
+                    pagenumber--;
+                    new JSONTask().execute("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/" +
+                            "in_theaters.json?page_limit=15&page=" + pagenumber +
+                            "&country=us&apikey=yedukp76ffytfuy24zsqk7f5");
+                }
+            }
+        });
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                StringBuffer finalBuffer = new StringBuffer();
+                String finalJSON = buffer.toString();
+                JSONObject parentObject = new JSONObject(finalJSON);
+                JSONArray parentArray = parentObject.getJSONArray("movies");
+                for (int i = 0; i < 10; i++) {
+                    JSONObject finalObject = parentArray.getJSONObject(i);
+                    String movieName = finalObject.getString("title");
+                    int year = finalObject.getInt("year");
+                    String newLine = movieName + " - " + year + "\n";
+                    finalBuffer.append(newLine);
+                }
+                return finalBuffer.toString();
+
+            } catch (IOException | JSONException exception) {
+                exception.printStackTrace();
+            } finally {
+                try {
+                    assert connection != null;
+                    connection.disconnect();
+                    assert reader != null;
+                    reader.close();
+                } catch (IOException | NullPointerException exception) {
+                    exception.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            rentalText.setText(s);
+        }
     }
 
 }
--- app/src/main/java/edu/gatech/snickers/techflixandchill/SearchMovies.java
+++ app/src/main/java/edu/gatech/snickers/techflixandchill/SearchMovies.java
@@ -2,13 +2,29 @@ package edu.gatech.snickers.techflixandchill;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SearchMovies extends Activity {
 
-    Button searchReturnToHome;
+    Button searchReturnToHome, searchButton;
+    TextView rentalText;
+    EditText search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,17 @@ public class SearchMovies extends Activity {
 
         final Bundle bundle;
         bundle = getIntent().getExtras();
+        searchButton = (Button)findViewById(R.id.searchButton);
+        rentalText = (TextView) findViewById(R.id.rentalText);
+        search = (EditText) findViewById(R.id.search_EDT);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String movieName = search.getText().toString();
+                new JSONTask().execute("http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=" +
+                        movieName + "&page_limit=10&page=1&apikey=yedukp76ffytfuy24zsqk7f5");
+            }
+        });
 
         searchReturnToHome = (Button) findViewById(R.id.searchReturnToHome);
 
@@ -29,6 +56,60 @@ public class SearchMovies extends Activity {
                 startActivity(i);
             }
         });
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                StringBuffer finalBuffer = new StringBuffer();
+                String finalJSON = buffer.toString();
+                JSONObject parentObject = new JSONObject(finalJSON);
+                JSONArray parentArray = parentObject.getJSONArray("movies");
+                for (int i = 0; i < 10; i++) {
+                    JSONObject finalObject = parentArray.getJSONObject(i);
+                    String movieName = finalObject.getString("title");
+                    int year = finalObject.getInt("year");
+                    String newLine = movieName + " - " + year + "\n";
+                    finalBuffer.append(newLine);
+                }
+                return finalBuffer.toString();
+
+            } catch (IOException | JSONException exception) {
+                exception.printStackTrace();
+            } finally {
+                try {
+                    assert connection != null;
+                    connection.disconnect();
+                    assert reader != null;
+                    reader.close();
+                } catch (IOException | NullPointerException exception) {
+                    exception.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            rentalText.setText(s);
+        }
     }
 
 }
